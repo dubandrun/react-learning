d1c8928cd73d0864b4556be01ea06bee5f220444
Anton Ivanov <starshine1215@gmail.com>
2023-04-08 18:06:42 +0300

    added changes
    updated webpack config
    added styles
    added theme
    added routing

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,29 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
 
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    },
+                }
+            },
+            "sass-loader",
+        ],
+    }
+
+    // Если не используем тайпскрипт - нужен babel-loader
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -10,5 +32,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
     return [
         typescriptLoader,
+        cssLoader,
     ]
 }
