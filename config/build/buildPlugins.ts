f7858a4197fe68347e00cde7851adba73e40c6a3
Anton Ivanov <starshine1215@gmail.com>
2023-06-11 20:31:29 +0300

    updated sidebar, button added loki cli

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -21,7 +21,6 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
-
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }));
